4ce0ba27164f68de675c9374b1a70427e1c127f1
s1stine <npl@duck.com>
2022-10-08 16:26:45 +0700

    get rekt boomer L bozo

--- age calculator.py
+++ age calculator.py
@@ -5,5 +5,7 @@ tuoi = input("nhập số tuổi của bạn: ")
 
 if tuoi.isnumeric():
     print("bạn đã", tuoi, "tuổi rồi")
+elif tuoi > 125:
+    print("con chào cụ, sao cụ già dữ v??????")
 else:
     print(tuoi, "tuổi là clgv??????")
